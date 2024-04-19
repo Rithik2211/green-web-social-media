ff80e427d29a83805bd8232b0a235d50043faa66
Rithik2211 <rithikpranao22@gmail.com>
2024-04-20 02:31:53 +0530

    initial erro struct

---
+++ green-web-app/pages/pages/index.js
@@ -0,0 +1,4 @@
+export {default as DashBoard} from './DashBoard';
+export {default as Explore} from './Explore';
+export {default as Notification} from './Notification';
+export {default as Messages} from './Messages';
