d3225ecf712aa9efba203f81260cf466f065c15c
Rithik2211 <rithikpranao22@gmail.com>
2024-11-06 22:33:07 +0530

    "Removed a block of code containing three motion.div elements with images and a heading, and kept a single motion.div element."

--- green-web-app/src/pages/Home.js
+++ green-web-app/src/pages/Home.js
@@ -6,32 +6,6 @@ import { fadeIn } from '../variants';
 function Home(){
     return(
     <div className='min-h-screen bg-white'>
-        {/* <div style={{display: "flex", flexWrap: "wrap", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
-            <motion.div 
-            variants={fadeIn('down',0.2)}
-            initial="hidden"
-            whileInView={"show"}
-            style={{width: "70%", height: "100%", borderRadius: "8px", margin: "20px"}}
-            >
-                <img src={sample}  alt='dashlogo'/>
-            </motion.div>
-            <motion.div 
-            variants={fadeIn('up',0.2)}
-            initial="hidden"
-            whileInView={"show"}
-            style={{width: "70%", height: "100%", borderRadius: "8px", margin: "20px"}}
-            >
-                <img src={women}  alt='avatar'/>
-            </motion.div>
-            <motion.div
-            variants={fadeIn('down',0.2)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{once: false, amount: 0.7}}
-            >
-                <h2 style={{fontFamily: "sans-serif"}}>Meet Some of the Users</h2>
-            </motion.div>
-        </div> */}
         <motion.div 
             variants={fadeIn('down',0.2)}
             initial="hidden"
